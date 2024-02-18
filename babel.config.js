ee97776ff011b4bb4459d2dedfaa658b57f8c31f
Anthony Cox <anthonyjohnc@gmail.com>
2024-02-18 13:37:20 +0000

    add initial babel configuration file

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+/**
+ * Developed by Anthony Cox in 2024
+ */
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        corejs: 3.36,
+        useBuiltIns: "usage",
+      }
+    ],
+    '@babel/preset-react'
+  ];
+
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-arrow-functions'
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
