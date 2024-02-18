5ddf5dd6ed5a0dfb0e556eedadf6b8ac507e8f8f
Anthony Cox <anthonyjohnc@gmail.com>
2024-02-18 13:36:50 +0000

    add babel configuration file

---
+++ src/babel-config.js
@@ -0,0 +1,35 @@
+/**
+ * Developed by Anthony Cox in 2024
+ */
+
+/**
+ * Configures Babel with all relevant settings for transpiling React components and ES6 JavaScript code
+ * @param {any} api
+ * @returns {{ presets: Array.<JSON>, plugins: Array.<string> }}
+ */
+const configureBabel = (api) => {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        corejs: 3.36,
+        useBuiltIns: "usage",
+      }
+    ],
+    '@babel/preset-react'
+  ];
+
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-arrow-functions'
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
+export default configureBabel;
