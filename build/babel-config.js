779b1076f6664fdb89d4f9eb2063ff350f3ee6a2
Anthony Cox <anthonyjohnc@gmail.com>
2024-02-18 13:38:33 +0000

    update build to latest version

---
+++ build/babel-config.js
@@ -0,0 +1,29 @@
+"use strict";
+
+require("core-js/modules/es.object.define-property.js");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+/**
+ * Developed by Anthony Cox in 2024
+ */
+
+/**
+ * Configures Babel with all relevant settings for transpiling React components and ES6 JavaScript code
+ * @param {any} api
+ * @returns {{ presets: Array.<JSON>, plugins: Array.<string> }}
+ */
+var configureBabel = function configureBabel(api) {
+  api.cache(true);
+  var presets = [['@babel/preset-env', {
+    corejs: 3.36,
+    useBuiltIns: "usage"
+  }], '@babel/preset-react'];
+  var plugins = ['@babel/plugin-proposal-class-properties', '@babel/plugin-syntax-dynamic-import', '@babel/plugin-transform-arrow-functions'];
+  return {
+    presets: presets,
+    plugins: plugins
+  };
+};
+var _default = exports["default"] = configureBabel;
---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+require("core-js/modules/es.object.define-property.js");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "configureBabel", {
+  enumerable: true,
+  get: function get() {
+    return _babelConfig["default"];
+  }
+});
+var _babelConfig = _interopRequireDefault(require("./babel-config"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
